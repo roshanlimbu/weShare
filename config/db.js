963bf2de25d4484cca94900bf98d93646c4c061f
roshanlimbu <topazkayle@gmail.com>
2023-09-10 23:57:43 +0545

    mongodb connected

--- config/db.js
+++ config/db.js
@@ -1,27 +1,26 @@
 require ("dotenv").config();
 const mongoose = require("mongoose");
 
-function dbConnect() {
+function DBconnect() {
   // Connect to the database
   mongoose.connect(process.env.MONGO_CONNECTION_URL, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: true, // Use `false` instead of `true` for `useFindAndModify`
-  });
+    // useFindAndModify: true, // Use `false` instead of `true` for `useFindAndModify`
+  }).then(()=> console.log("MongoDB connecton established."))
+    .catch((error)=>console.log("MongoDB connection error:", error.message));
 
-  const connection = mongoose.connection;
+  // const connection = mongoose.connection;
 
   // connection.on("error", (err) => {
   //   console.error("MongoDB connection error:", err);
   // });
 
-  connection.once("open", () => {
-    console.log("Connected to MongoDB");
-  }).catch(err=>{
-      console.log("MongoDB connection error:", err);
-    });
-}
+  // connection.once("open", () => {
+  //   console.log("Connected to MongoDB");
+  // });
+};
 
-module.exports = dbConnect;
+module.exports = DBconnect;
 
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 
 let PORT =  process.env.PORT || 3000;
 
+const connectDB = require("./config/db");
+connectDB();
+
 app.listen(PORT, ()=>{
   console.log(`Listening on port ${PORT}`);
 })
-// uMjzdI5FKwNa5yOE
+//3REsIjkwNRTSkA8s 
